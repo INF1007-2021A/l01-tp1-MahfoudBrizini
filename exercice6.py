39129c2dc344ed593e6e17f4b0bee56019bdd4e7
hainsdominic <contact.dominichains@gmail.com>
2021-09-17 18:33:41 -0400

    correction 6

--- exercice6.py
+++ exercice6.py
@@ -26,9 +26,12 @@ def effectuerRotation(nombreComplexe, angle_rotation, trouverModule):
 
     # TODO: Calculer le nouveau nombre complexe après rotation, assigner le nouveau nombre complexe à la variable 'resultat'
 
-    resultat = complex(
-        nombreComplexe.real * math.cos(math.radians(angle_rotation)),
-        nombreComplexe.imag * math.sin(math.radians(angle_rotation)),
+    resultat = (
+        complex(
+            math.cos(math.radians(angle_rotation)),
+            math.sin(math.radians(angle_rotation)),
+        )
+        * nombreComplexe
     )
 
     nouveauModule = trouverModule(resultat)
